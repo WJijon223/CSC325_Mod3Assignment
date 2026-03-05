808e8dafc306ca623ad61da7cd464559f71eb1f8
Cyrus-Hossain <cyrusnydhaka@gmail.com>
2026-03-05 16:04:05 -0500

    Populate staffList with 6 employees in Staff constructor

--- src/main/java/com/mycompany/agency/Staff.java
+++ src/main/java/com/mycompany/agency/Staff.java
@@ -1,29 +1,32 @@
 package com.mycompany.agency;
 
-
-
-
 public class Staff
 {
     private StaffMember[] staffList;
+//-----------------------------------------------------------------
+//  Constructor: Sets up the list of staff members.
+//-----------------------------------------------------------------
+public Staff()
+{
+    staffList = new StaffMember[6];
 
-    //-----------------------------------------------------------------
-    //  Constructor: Sets up the list of staff members.
-    //-----------------------------------------------------------------
-    public Staff()
-    {
-        staffList = new StaffMember[6];
+    // TODO 1: Add 6 employees to the staffList
 
-        // TODO 1: Add 6 emploees to the staffList
+    staffList[0] = new Executive("Sam", "123 Main Line", "555-0469", "123-45-6789", 2423.07);
+    staffList[1] = new StaffEmployee("Carla", "456 Off Line", "555-0101", "987-65-4321", 1246.15);
+    staffList[2] = new StaffEmployee("Woody", "789 Off Rocker", "555-0000", "010-20-3040", 1169.23);
+    staffList[3] = new TempEmployee("Diane", "678 Fifth Ave.", "555-0690", "958-47-3625", 10.55);
+    staffList[4] = new Intern("Norm", "987 Suds Blvd.", "555-8374");
+    staffList[5] = new Intern("Cliff", "321 Duds Lane", "555-7282");
 
-        ((Executive)staffList[0]).awardBonus(500.00);
-
-        ((TempEmploee)staffList[3]).addHours(40);
-    }
+    ((Executive)staffList[0]).awardBonus(500.00);
+    ((TempEmployee)staffList[3]).addHours(40);
+}
 
     //-----------------------------------------------------------------
     //  Pays all staff members.
     //-----------------------------------------------------------------
+
     public void payday ()
     {
         double amount;
